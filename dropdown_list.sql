4d347c9681edee12627081e138c61a95f882481b
vukm <vukm@miamioh.edu>
2023-12-03 16:34:45 -0500

    c

--- dropdown_list.sql
+++
@@ -1,7 +0,0 @@
-select distinct rating from movies.amazon
-union
-select distinct rating from movies.disney
-union
-select distinct rating from movies.hulu
-union
-select distinct rating from movies.netflix;
